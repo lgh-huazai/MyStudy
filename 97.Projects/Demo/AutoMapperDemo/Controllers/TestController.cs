8a62c2e0aaa90868bbb205836fe083a2b4274ccd
huazai <1297933709@qq.com>
2022-07-17 18:01:44 +0800

    add demo .slu

---
+++ 97.Projects/Demo/AutoMapperDemo/Controllers/TestController.cs
@@ -0,0 +1,34 @@
+using AutoMapper;
+using AutoMapperDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net;
+
+namespace AutoMapperDemo.Controllers
+{
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public TestController() {}
+
+        [HttpPost]
+        [Route("TestAutoMapper")]
+        [SwaggerResponse((int)HttpStatusCode.OK, "Response Model", typeof(UIModel))]
+        public IActionResult TestAutoMapper([FromBody] ORModel ormData)
+        {
+            // 1.映射规则配置
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ORModel, UIModel>()
+                    .ForMember(ui => ui.Gender, opt => opt.MapFrom(orm => orm.Gender == 1 ? "男" : "女"))
+                    .ForMember(ui => ui.StudentName, opt => opt.MapFrom(orm => orm.Name))
+                    .ForMember(ui => ui.BirthDay, opt => opt.MapFrom(orm => orm.BirthDay.ToString("yyyy-MM-dd HH:mm:ss"))); 
+            });
+            // 2.创建映射的实例
+            var mapper = mapperConfig.CreateMapper();
+            // 3.数据映射
+            var uiData = mapper.Map<UIModel>(ormData);
+            return Ok(uiData);
+        }
+    }
+}
